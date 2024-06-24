954de15af03cce9f035879996a6f2177cabb0bed
Andy Byers <a.byers@bbk.ac.uk>
2024-06-24 13:11:59 +0100

    #7 adds granular control over doi minting

--- urls.py
+++ urls.py
@@ -1,10 +1,20 @@
-from django.conf.urls import url
+from django.urls import re_path
+from django.views.generic import TemplateView
 
 from plugins.datacite import views
 
-
 urlpatterns = [
-    url(r'^articles/$', views.article_list, name='datacite_articles'),
-    url(r'^articles/export/(?P<article_id>\d+)/$', views.article_export, name='datacite_article_export'),
-    url(r'^articles/(?P<article_id>\d+)/mint/$', views.add_doi, name='datacite_add_doi'),
+    re_path(
+        r'^$',
+        TemplateView.as_view(template_name="datacite/index.html"),
+        name='datacite_index',
+    ),
+    re_path(r'^manager/$', views.manager, name='datacite_manager'),
+    re_path(r'^articles/$', views.article_list, name='datacite_articles'),
+    re_path(r'^articles/export/(?P<article_id>\d+)/$', views.article_export,
+            name='datacite_article_export'),
+    re_path(r'^articles/(?P<article_id>\d+)/mint/$', views.add_doi,
+            name='datacite_add_doi'),
+    re_path(r'^sections/$', views.section_mint_manager,
+            name='datacite_section_mint_manager'),
 ]
